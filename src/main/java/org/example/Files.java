9c409662085d3fa1eefb0624d5d691d187e9d531
kawinkumar25 <123533543+kawinkumar25@users.noreply.github.com>
2023-02-28 14:48:18 +0530

    Update Files.java

--- src/main/java/org/example/Files.java
+++ src/main/java/org/example/Files.java
@@ -14,7 +14,8 @@ public class Files
         StringBuilder str = new StringBuilder();
         try
         {
-            File file = new File("C:\\Users\\Tringapps-User21\\data_analyst.txt");
+            String s = "C:\\Users\\Tringapps-User21\\data_analyst.txt";
+            File file = new File(s);
             Scanner inp = new Scanner(file);
             while(inp.hasNextLine())
             {
@@ -28,32 +29,8 @@ public class Files
             l.info(s);
         }
         String[] str1 = str.toString().split(" ");
-        HashMap<String ,Integer> hash= new HashMap<>();
-
-        for(int i=0;i< str1.length;i++)
-        {
-            if(hash.containsKey(str1[i]))
-            {
-                hash.put(str1[i],hash.get(str1[i])+1);
-            }
-            else
-            {
-                hash.put(str1[i],1);
-            }
-        }
-        l.log(Level.INFO, ()->("Frequncy "+hash));
-        l.log(Level.INFO, ()->("Size "+str1.length));
-
-        PriorityQueue<HashMap.Entry<String, Integer>> q = new PriorityQueue<>((a, b) -> {
-            return b.getValue() - a.getValue();
-        });
-
-        q.addAll(hash.entrySet());
-
-        while (!q.isEmpty()) {
-            l.log(Level.INFO,() ->"\n" + q.poll());
-        }
-
+        Fileslogic obj = new Fileslogic(str1);
+        obj.hash();
 
     }
 }
